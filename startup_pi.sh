ca784f4c821d4470f84b30cc5de30339e8b39451
Taras <taras19041@gmail.com>
2024-11-08 23:41:20 +0200

    Update startup_pi.sh
    Add k3s and gh install

--- startup_pi.sh
+++ startup_pi.sh
@@ -2,3 +2,5 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+curl -sfL https://get.k3s.io | sh -
+sudo apt install gh
