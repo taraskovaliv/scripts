db1e9f05940760a9fde1b8f63b66c1e7aef4f375
Taras Kovaliv <taraskovaliv@outlook.com>
2024-11-17 22:12:18 +0200

    Update registry creation.

--- startup_pi.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install software-properties-common -y
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-curl -sfL https://get.k3s.io | sh -
-sudo apt install gh
